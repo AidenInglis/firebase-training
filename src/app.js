e00507a12c479501a8c478d0f8fba61d439ac432
AidenInglis <aiden-inglis@live.nmit.ac.nz>
2023-10-19 11:10:25 +1300

    commit-for-firebase

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//TODO - App View
+
+const leftCol = document.getElementById('left-col');
+const backBtn = document.getElementById('back-btn');
+
+backBtn.addEventListener('click', (e) => {
+    leftCol.style.display = "block";
+    rightCol.style.display = "none";
+});
