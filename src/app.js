172bfbfbc38d5f722f3f86c15f4578b6d9023141
AidenInglis <aiden-inglis@live.nmit.ac.nz>
2023-10-19 11:14:22 +1300

    commit-for-firebase

--- src/app.js
+++ src/app.js
@@ -7,3 +7,14 @@ backBtn.addEventListener('click', (e) => {
     leftCol.style.display = "block";
     rightCol.style.display = "none";
 });
+
+const toggleLeftAndRightViewsOnMobile = () => {
+    if (document.body.clientWidth <= 600) {
+        leftCol.style.display = "none";
+        rightCol.style.display = "block";
+    }
+}
+
+//SECTION - Data
+
+let contacts = [];
