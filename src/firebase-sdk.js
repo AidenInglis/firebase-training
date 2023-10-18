c02bb4fd40310ff302bb935525ae9af3dd4f7aa4
AidenInglis <aiden-inglis@live.nmit.ac.nz>
2023-10-19 10:51:52 +1300

    commit-for-firebase

---
+++ src/firebase-sdk.js
@@ -0,0 +1,18 @@
+//TODO - Import the functions from the firebase SDKs.
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js"
+//TODO - Add SDKs for firebase products.
+
+//NOTE - Your web app's Firebase Config.
+
+const firebaseConfig = { 
+    apiKey: "AIzaSyB18UyOC-QI9cGHjg9zFhTMqUeay8HYwUE",
+    authDomain: "fir-training-80467.firebaseapp.com",
+    projectId: "fir-training-80467",
+    storageBucket: "fir-training-80467.appspot.com",
+    messagingSenderId: "538344072118",
+    appId: "1:538344072118:web:971b220f9206801e225717",
+    //measurementId: "";
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
